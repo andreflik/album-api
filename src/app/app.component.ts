078c410fd66195210dee6851a4743e9c5817e5d4
Andre Fagundes <andreflik@gmail.com>
2021-03-25 21:28:21 -0300

    API album de fotos

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Post } from './post';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
+import { PostService } from './post.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  private posts: Post[];
+  
+    constructor(
+      public dialog: MatDialog,
+      public postService: PostService)  {}
+      
+
+
+  ngOnInit() {
+      this.posts = this.postService.posts;
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(PostDialogComponent, {
+      width: '600px',
+      
+    });
+    dialogRef.afterClosed().subscribe(
+      (result)=> {
+        if (result) {
+         this.postService.salvar(result.post, result.arquivo);
+        }
+      }
+    );
+      
+      
+  }
+}
